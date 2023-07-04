9c98c13ab4a5f20aefcc4e41eef5d858633026b9
yumaon <110388364+yumaon@users.noreply.github.com>
2023-07-04 22:55:34 +0900

    修正

--- paiza_c14.php
+++ paiza_c14.php
@@ -1,20 +1,17 @@
 <?php
 $c = trim(fgets(STDIN));
-foreach(range(1, $c) as $i) {
-  $array[] = explode(" ", trim(fgets(STDIN)));
-}
-
 $point = 0;
-foreach(range(0, ($c - 1)) as $i) {
-  $day_info = str_split($array[$i][0]);
+foreach(range(1, $c) as $i) {
+  $input = explode(" ", trim(fgets(STDIN)));
+  $day_info = str_split($input[0]);
+  $price = $input[1];
   if (in_array(3, $day_info)) {
-    $point += floor($array[$i][1] * 0.03);
+    $point += floor($price * 0.03);
   } elseif (in_array(5, $day_info)) {
-    $point += floor($array[$i][1] * 0.05);
+    $point += floor($price * 0.05);
   } else {
-    $point += floor($array[$i][1] * 0.01);
+    $point += floor($price * 0.01);
   }
 }
-
 echo $point."\n";
 ?>
